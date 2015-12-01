3df3f22e01f7db6e1df38fbffd54e4fec0d890ac
Anurag Kyal <anuragkyal2008@gmail.com>
2015-12-01 00:23:05 -0500

    Added new endpoints

--- dataservice/fetch_comments.py
+++ dataservice/fetch_comments.py
@@ -3,11 +3,14 @@ import json
 import csv
 import urllib2
 import subprocess
-import glob, os
+import os, os.path
+import random
 
 urls = (
 	'/fetch_comments', 'Fetch',
-	'/test', 'Test'
+	'/test', 'Test',
+	'/get_count', 'Count',
+	'/get_top', 'Top'
 )
 
 class Fetch:
@@ -22,20 +25,23 @@ class Fetch:
 		cr = csv.reader(response)
 
 		print "reading file"
+		index = 1
 
 		for row in cr:
-			if len(comment_urls) is 100:
+			if len(comment_urls) is 10000:
 				break
 
-			if (len(row) >= 35 and row[34].startswith('http://')):
+			if index > 15000 and (len(row) >= 35 and row[34].startswith('http://')):
 				comment_urls.append(row[34])
+
+			index += 1
 		
 		print len(comment_urls)
 		self.extract_comments(comment_urls)
 		return 'success'
 
 	def extract_comments(self, comments_url):
-		i = 1
+		i = 15000
 		self.empty_dir('../comments')		
 		for url in comments_url:
 			print url
@@ -52,6 +58,36 @@ class Test:
 		res = subprocess.call(['/Users/anurag/study/SEM-3/bds/proj/opt/dataService/phantomjs-2.0.0-macosx/bin/phantomjs', '/Users/anurag/study/SEM-3/bds/proj/opt/dataService/test.js', 'http://www.regulations.gov/#!documentDetail;D=ICEB-2015-0002-2258', ])
 		print res
 		return 'success'
+
+class Count:
+	def GET(self):
+		DIR = '../comments'
+		res = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+		return res
+
+class Top:
+	def GET(self):
+		DIR = '../comments'
+		paths = [name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]
+		contents = []
+		res = []
+
+		for path in paths:
+			text_file = open('../comments/' + path, "r")
+			whole_thing = text_file.read()
+			contents.append(whole_thing)
+		
+		size = len(contents)
+		print size
+		if size < 100:
+			return contents
+		else:
+			for i in range(100):
+				index = random.randint(0, size-1)
+				print index
+				res.append(contents[index])
+
+			return res		
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
